c4bab0280a18f4861b7a542b453a968094b96cba
rocconess85-ship-it <rocconess85@gmail.com>
2025-08-27 15:03:59 -0400

    Update sslstrip.py

--- sslstrip.py
+++ sslstrip.py
@@ -9,6 +9,19 @@ Copyright (c) 2004-2009 Moxie Marlinspike <moxie@thoughtcrime.org>
  
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
+
+
+
+
+
+
+
+
+
+
+
+
+
 published by the Free Software Foundation; either version 3 of the
 License, or (at your option) any later version.
 
